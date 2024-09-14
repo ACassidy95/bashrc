71a6530482c7f31c9687853ed6653674a7bcb19a
aaronc <aaron.cassidy1995@gmail.com>
2024-09-14 22:11:37 +0100

    Add shorthand for git status

--- git.sh
+++ git.sh
@@ -10,3 +10,17 @@ alias gcan="git commit --amend --no-edit"
 
 alias gps="git push"
 alias gpl="git pull"
+
+alias gst="git status"
+
+# Traverse to the top level of the current git repo
+function gcdr() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == true ]]; then
+    groot=$(git rev-parse --show-toplevel)
+    cd "$groot" || return
+  else
+    echo "$(pwd) is not a Git repo :("
+  fi
+}
+
+export -f gcdr

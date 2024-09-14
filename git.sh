19f4099845494b3f8fc137d348ef0eaf54f1f2f6
aaronc <aaron.cassidy1995@gmail.com>
2024-09-14 22:08:14 +0100

    Add git shorthand

---
+++ git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+alias g="git"
+
+alias gad="git add"
+
+alias gcm="git commit -m"
+alias gca="git commit --amend"
+alias gcan="git commit --amend --no-edit"
+
+alias gps="git push"
+alias gpl="git pull"
